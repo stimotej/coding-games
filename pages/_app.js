19ce7e754d5d8134f786e84e1c7e9e64ff47e21f
Timotej Sofijanović <76438562+stimotej@users.noreply.github.com>
2023-03-09 17:00:22 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { SWRConfig } from "swr";
 import "../styles/globals.css";
 
 // axios.defaults.baseURL = "http://localhost:5000/api";
-axios.defaults.baseURL = "https://coding-games-mc2.herokuapp.com/api";
+axios.defaults.baseURL = "https://coding-games-backend.onrender.com/api";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
